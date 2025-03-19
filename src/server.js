59319c2ec7043022fe18c36193e7ee9b5ab92233
sahilshivekar <sahilshivekarce@gmail.com>
2025-03-19 19:45:33 +0530

    Add self ping for server and modifiy the frequency of self ping for db

--- src/server.js
+++ src/server.js
@@ -7,27 +7,26 @@ const port = process.env.PORT || 8000;
 
 
 // for stoping the server instance on render to go down with inactivity
-// app.get("/api/v1/self-ping", async (req, res) => {
-//     res.status(200).send(null);
-// })
+app.get("/api/v1/self-ping", async (req, res) => {
+    res.status(200).send(null);
+})
 
-// setInterval(() => {
-//     https.get(process.env.SERVER_ADDRESS + "/api/v1/self-ping", (res) => {
-//         console.log("Self-ping: ", res.statusCode)
-//     }).on('error', (err) => {
-//         console.log("Error during self-ping: ", err.message)
-//     })
-// }, 1000 * 60 * 3) // 3 minutes
+setInterval(() => {
+    https.get(process.env.SERVER_ADDRESS + "/api/v1/self-ping", (res) => {
+        console.log("Server Self-ping: ", res.statusCode)
+    }).on('error', (err) => {
+        console.log("Error during self-ping: ", err.message)
+    })
+}, 1000 * 60 * 2) //2 minutes
 
 
 //for stoping the database service from going down with inactivity
 setInterval(async () => {
     const users = await sequelize.query(`SELECT user_id FROM users LIMIT 1;`)
     if (users) {
-        console.log("DB frequent ping")
-        // console.log(users)
+        console.log("DB Self-ping: ")
     }
-}, 1000 * 10) // 10 seconds
+}, 1000 * 20) // 20 seconds
 
 app.listen(port, () => {
     console.log(`Server is running at port: ${port}`);
